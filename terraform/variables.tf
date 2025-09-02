ee2458f770051c243ac5e16f7572248a357a3c19
mustafa3731 <111272216+mustafa3731@users.noreply.github.com>
2025-09-02 14:40:59 -0400

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,39 @@
-variable "rg_name"        { type = string  default = "demo-vm-rg" }
-variable "location"       { type = string  default = "eastus" }
-variable "vm_name"        { type = string  default = "demo-vm" }
-variable "vm_size"        { type = string  default = "Standard_B1s" }
-variable "admin_username" { type = string  default = "azureuser" }
-variable "admin_password" { type = string  sensitive = true }
+variable "rg_name" {
+  type    = string
+  default = "demo-vm-rg"
+}
 
-variable "vnet_cidr"      { type = string  default = "10.10.0.0/16" }
-variable "subnet_cidr"    { type = string  default = "10.10.1.0/24" }
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "demo-vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "vnet_cidr" {
+  type    = string
+  default = "10.10.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.10.1.0/24"
+}
