e38c87c151a6b08f0edc876b3170c6a45eac3c12
mustafa3731 <111272216+mustafa3731@users.noreply.github.com>
2025-09-02 15:07:17 -0400

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP of the VM"
-  value       = azurerm_public_ip.pip.ip_address
-}
-
-output "http_endpoint" {
-  description = "Apache endpoint"
-  value       = "http://${azurerm_public_ip.pip.ip_address}"
-}
