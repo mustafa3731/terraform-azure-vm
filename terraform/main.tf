61194363d3c68ca9be6cdd7abc6effe4d4962544
mustafa3731 <111272216+mustafa3731@users.noreply.github.com>
2025-09-02 14:36:23 -0400

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,149 @@
-variable "rg_name" {
-  type    = string
-  default = "demo-vm-rg"
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  # Remote state in Azure Blob Storage (BACKEND)
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "mustafastatestorage"   # your real storage account
+    container_name       = "tfstate"
+    key                  = "azure-vm/terraform.tfstate"
+  }
 }
 
-variable "location" {
-  type    = string
-  default = "eastus"
+provider "azurerm" {
+  features {}
 }
 
-variable "vm_name" {
-  type    = string
-  default = "demo-vm"
+# ------------------------
+# Resource Group
+# ------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
 }
 
-variable "vm_size" {
-  type    = string
-  default = "Standard_B1s"
+# ------------------------
+# Networking
+# ------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.rg_name}-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.vnet_cidr]
 }
 
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.rg_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_cidr]
 }
 
-variable "admin_password" {
-  type      = string
-  sensitive = true
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.vm_name}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-variable "vnet_cidr" {
-  type    = string
-  default = "10.10.0.0/16"
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-variable "subnet_cidr" {
-  type    = string
-  default = "10.10.1.0/24"
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# ------------------------
+# Linux VM + Apache via provisioner
+# ------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  # Demo: password login (set via TF_VAR_admin_password secret)
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    name                 = "${var.vm_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y apache2",
+      "sudo systemctl enable --now apache2",
+      "echo 'Hello from Terraform!' | sudo tee /var/www/html/index.html"
+    ]
+    connection {
+      type     = "ssh"
+      host     = azurerm_public_ip.pip.ip_address
+      user     = var.admin_username
+      password = var.admin_password
+      timeout  = "5m"
+    }
+  }
 }
